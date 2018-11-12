acc00a0567146296e7c4c6e66b84a13814a09e77
Oussama Bougaila <oussama.bougaila@gmail.com>
2018-11-12 09:42:13 +0100

    feat: Add reserveBus methode ReserveBus will create a bus and move to createtrip page

---
+++ src/com/telly/controllers/BusController.java
@@ -0,0 +1,34 @@
+package com.telly.controllers;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.telly.dao.Bus;
+import com.telly.dao.FormValidationGroup;
+import com.telly.service.BusService;
+
+@Controller
+public class BusController {
+
+	@Autowired
+	BusService busService;
+	
+
+	@RequestMapping("/createtrip")
+	public String reserveBus(Model model, Principal principal) {
+
+		model.addAttribute("bus", new Bus());
+
+		return "createtrip";
+	}
+	
+
+}
